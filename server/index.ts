2b97c8da77f87b8022ede7e6906d11e3fa361029
Tommi Heinonen <tommi.heinonen@student.lut.fi>
2025-02-10 17:28:37 +0200

    changed serverside port and tried a new mongodb

--- server/index.ts
+++ server/index.ts
@@ -8,9 +8,9 @@ import cors, {CorsOptions} from 'cors'
 dotenv.config()
 
 const app: Express = express()
-const port: number = 3000
+const port: number = 1234
 
-const mongoDB: string = "mongodb://127.0.0.1:27017/testdb"
+const mongoDB: string = "mongodb://localhost:27017/testdb"
 mongoose.connect(mongoDB)
 mongoose.Promise = Promise
 const db: Connection = mongoose.connection
