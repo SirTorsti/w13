812939d954fddcde55f111f0f484933ae773c9e0
Tommi Heinonen <tommi.heinonen@student.lut.fi>
2025-02-10 17:47:29 +0200

    didn't save one of the files..

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -17,4 +17,17 @@ router.post('/api/book', async (req: Request, res: Response) => {
     }
 })
 
+router.get('/api/book/:name', async (req: Request, res: Response) => {
+    try {
+        const book = await Book.findOne({name: req.params.name})
+        if(!book) {
+            res.status(404).json({message: 'Book not found'})
+        }
+    } catch (error: any) {
+        console.error(`Error while fetching book: ${error.message}`)
+        res.status(500).json({message: 'Internal server error'
+        })
+    }
+})
+
 export default router
